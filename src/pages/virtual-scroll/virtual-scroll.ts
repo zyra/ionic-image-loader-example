dffb445b63387a58d736027fb703038320736fe4
Ibby <ibby93@gmail.com>
2017-02-12 10:33:09 -0500

    update virtual scroll images

--- src/pages/virtual-scroll/virtual-scroll.ts
+++ src/pages/virtual-scroll/virtual-scroll.ts
@@ -6,14 +6,20 @@ import { Component } from '@angular/core';
 })
 export class VirtualScrollPage {
 
-  images: string[] = [];
+  // images taken from pexels.com
+  images: string[] = [
+    'https://images.pexels.com/photos/27986/pexels-photo-27986.jpg?h=350&auto=compress&cs=tinysrgb',
+    'https://images.pexels.com/photos/115010/tulips-red-red-tulips-garden-115010.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://images.pexels.com/photos/206393/pexels-photo-206393.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://images.pexels.com/photos/316591/pexels-photo-316591.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://images.pexels.com/photos/274060/pexels-photo-274060.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://images.pexels.com/photos/126345/pexels-photo-126345.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://images.pexels.com/photos/52977/coffee-coffee-maker-restaurant-cafe-52977.jpeg?h=350&auto=compress&cs=tinysrgb'
+  ];
+
+
 
   constructor() {
-    for (let i = 0; i < 20; i++) {
-      const url = "http://lorempixel.com/200/200/abstract/?v=" + Date.now() + i;
-      this.images.push(url);
-    }
-    console.log(this.images);
   }
 
 }
